233e38040e3230445bd16dd42a1cd9f410603ef2
Marco Foi <foimarco@gmail.com>
2023-06-01 18:01:11 +0200

    Improving camera configuration for fitting actual depth capturing scenario

--- framedumper.py
+++ framedumper.py
@@ -2,23 +2,50 @@ import depthai as dai
 
 pipeline = dai.Pipeline()
 
-# Define XLinkIn 'control' node
-controlInNode = pipeline.create(dai.node.XLinkIn)
-controlInNode.setStreamName('controlInNode')
-
 # Create MonoCamera nodes
 monoRight = pipeline.create(dai.node.MonoCamera)
+# monoRight = pipeline.createMonoCamera() # Same as above using factory method
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 monoLeft = pipeline.create(dai.node.MonoCamera)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 
 # Create StereoDepth node
 stereo = pipeline.create(dai.node.StereoDepth)
+
+# https://docs.luxonis.com/projects/api/en/latest/components/nodes/stereo_depth/#currently-configurable-blocks
+
+# To prioritize fill-rate, sets Confidence threshold to 245
+# To prioritize accuracy, sets Confidence threshold to 200 .. ..or use: stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
+threshold = 200 # Can range between 0:max confidence - 255:min confidence
+stereo.initialConfig.setConfidenceThreshold(threshold)
+# 
+stereo.initialConfig.setDisparityShift(50) # This will reduce MazZ to ~140 cm
+# LeftRightCheck(True) = Better handling for occlusions but has a performance cost
+stereo.setLeftRightCheck(True)
+# ExtendedDisparity(True) = Closer-in minimum depth, disparity range is doubled:
+stereo.setExtendedDisparity(True)
+# Subpixel(True) = Better accuracy for longer distance, fractional disparity 32-levels:
+stereo.setSubpixel(True)
+
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_ACCURACY)
 stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout from rectification (black stripe on the edges)
 
 
-# Link MonoCameras to StereoCamera node
+# Link MonoCameras to StereoCamera node without creating explicitly a XLinkOut for each of the mono cameras
 monoRight.out.link(stereo.right)
 monoLeft.out.link(stereo.left)
+
+# Create a XLinkOut and link it to StereoCamera output
+stereoXlinkOut = pipeline.createXLinkOut()
+stereoXlinkOut.setStreamName("stereoXlinkOut")
+
+# Create explicit 'XLinkOut' for each Mono camera node and attach it
+monoRightXlinkOut = pipeline.create(dai.node.XLinkOut) # The out of a XLinkOut is a Queue!
+monoRightXlinkOut.setStreamName("monoRightOut")
+monoRight.out.link(monoRightXlinkOut.input)
+monoLeftXlinkOut = pipeline.create(dai.node.XLinkOut) # The out of a XLinkOut is a Queue!
+monoLeftXlinkOut.setStreamName("monoLeftOut")
+monoRight.out.link(monoLeftXlinkOut.input)
 
 # Create ImageManip node
 manip = pipeline.create(dai.node.ImageManip)
@@ -31,15 +58,24 @@ colorCamera.setInterleaved(False)
 colorCamera.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 colorCamera.setIsp3aFps(2)
 
+# Define XLinkIn 'control' node
+controlInNode = pipeline.create(dai.node.XLinkIn)
+controlInNode.setStreamName('controlInNode')
 # Link output of 'control' node to inputControl of ColorCamera node
 controlInNode.out.link(colorCamera.inputControl)
 
 # Create XLinkOut node
-xlinkOut = pipeline.create(dai.node.XLinkOut)
-xlinkOut.setStreamName('cameraOutput')
+colorXlinkOut = pipeline.create(dai.node.XLinkOut)
+colorXlinkOut.setStreamName('cameraOut')
 
-colorCamera.isp.link(xlinkOut.input)
+colorCamera.still.link(colorXlinkOut.input) # The out of a XLinkOut is a Queue!
 
+with dai.Device(pipeline) as device:
+    # Print MxID, USB speed, and available cameras on the device
+    print('MxId:', device.getDeviceInfo().getMxId())
+    print('USB speed:', device.getUsbSpeed())
+    print('Connected cameras:', device.getConnectedCameras())
+    #device.startPipeline()
 
 '''
 configInNode = pipeline.create(dai.node.XLinkIn)
@@ -58,7 +94,7 @@ videoOutNode.setStreamName('video')
 stillMjpegOutNode.setStreamName('still')
 '''
 
-
+'''
 # Upload the pipeline to the device
 with dai.Device(pipeline) as device:
     # Print MxID, USB speed, and available cameras on the device
@@ -74,6 +110,7 @@ with dai.Device(pipeline) as device:
     cameraControl.setCaptureStill(True)
     # Send a message to controlInQueue
     controlInQueue.send(cameraControl)
+'''
 
 # colorCamera.setPreviewSize(300, 300)
 # colorCamera.setInterleaved(False)
